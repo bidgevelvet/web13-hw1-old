7eef9e8926126c1c76632ca4e6e0a13ebbbe04de
Decha Tesapirat <decha_big@hotmail.com>
2013-08-25 22:01:39 +0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mybook::Application.config.secret_token = 'd99ff2a324b5fc9296e3c67fa7ccd8f3373778bc37c6d0258198b1a753318ba8211b036b17d253412655234d591ed1eb4e427743a86c78af1be47414d5e72338'
